6825be91ce2946658bea12da06fbde0c2b9feb86
Kwounsu <sb00lgs@gmail.com>
2020-06-10 13:06:13 -0700

    first commit

---
+++ app/src/main/java/com/example/tablayoutwithviewpager/SportFragment.kt
@@ -0,0 +1,18 @@
+package com.example.tablayoutwithviewpager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SportFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sport, container, false)
+    }
+
+}
